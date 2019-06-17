fd70985cfe887a34202811f20df40e30f1b649f5
DuHo3aBp <ltkcinfo@gmail.com>
2019-06-18 00:40:25 +0300

    commit

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 #print ("Hello Oleg!")
-from os import getcwd
-where_am_I=getcwd()
+import os
+import datetime
+import time
+import html
+#print(os.getcwd())
+#print(os.getenv('HOME'))
+#print(datetime.date.isoformat(datetime.date.today()))
+print(time.strftime("%H:%M %A %p"))
+
+#Преобразование разметки html и экранированный текст и обратно
+print(html.escape("this html fragment contains <script>script</script> tag."))
+print(html.unescape("I &hearts; Pyton &lt;standats&gt;."))
